1dbe213a3e147ad95dc9ede604fa975f9a6fa653
Tony Guo <tonyguo0815@gmail.com>
2020-03-09 17:04:17 +0800

    react expree bsaic_type

---
+++ lab9_basic_type/demo1.js
@@ -0,0 +1,6 @@
+var x1 = 5
+var x2 = 'Hello'
+var x3
+var x4 = null
+x2 = null
+console.log(x1, x2, x3, x4)
---
+++ lab9_basic_type/demo2.js
@@ -0,0 +1,8 @@
+var x = 5
+var y = 3.14
+var z = true
+var p = 'Hello'
+var q = "Hello"
+var t = [1, 2, 3]
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof t)
+console.log(x, y, z, p, q, t)
