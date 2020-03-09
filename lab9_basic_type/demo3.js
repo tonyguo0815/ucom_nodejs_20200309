2a83722e869beddb014d110adb2dc0924bc80d25
Tony Guo <tonyguo0815@gmail.com>
2020-03-09 17:31:06 +0800

    0309 end

---
+++ lab9_basic_type/demo3.js
@@ -0,0 +1,6 @@
+var x1 = {}
+var x2 = []
+var x3 = [1, 2, 3]
+function x4() { }
+console.log(x1, x2, x3, x4)
+console.log(Array.isArray(x1),Array.isArray(x2),Array.isArray(x3),Array.isArray(x4))
---
+++ lab9_basic_type/demo4.js
@@ -0,0 +1,3 @@
+console.log(512 * 512)
+console.log(2147483647 + 9999999999999999)
+console.log(0.4 + 0.2)
